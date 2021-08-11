8157b69c5fb041e14833e3f76e4923a169392b18
satakan35 <satakan35@gmail.com>
2021-08-11 19:46:07 -0400

    second commit

---
+++ src/gitfinalclass/GitClassFile02.java
@@ -0,0 +1,12 @@
+package gitfinalclass;
+
+public class GitClassFile02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is second commit file");
+
+
+	}
+
+}
